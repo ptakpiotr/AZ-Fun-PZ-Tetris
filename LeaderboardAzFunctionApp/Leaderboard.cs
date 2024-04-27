f8740fb8e950d131bc551aa9521feb7ed94efadd
ptakpiotr <piter020@outlook.com>
2024-04-27 11:39:44 +0200

    Change request response

--- LeaderboardAzFunctionApp/Leaderboard.cs
+++ LeaderboardAzFunctionApp/Leaderboard.cs
@@ -42,7 +42,7 @@ public static class Leaderboard
         {
             logger.LogError(ex, ex.Message);
 
-            return new ObjectResult(new ResultModel("Error occured while trying to add the new score"))
+            return new ObjectResult(new ResultModel(ex.Message))
             {
                 StatusCode = StatusCodes.Status500InternalServerError
             };
